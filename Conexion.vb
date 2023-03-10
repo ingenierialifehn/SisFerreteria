b7c232c9bf6b4d4a47582a96c6aba52af4a21d12
ingenierialifehn <77239250+ingenierialifehn@users.noreply.github.com>
2023-03-10 13:53:15 -0600

    Add files via upload

---
+++ Conexion.vb
@@ -0,0 +1,22 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Data.Sql
+Imports System.Data.SqlClient
+Module Conexion
+    Public con As SqlConnection
+    Public obtenerIdCliente As Integer = 1
+    Public obtenerIdProducto As Integer
+    Public obtenerTelefonoCliente As String
+    Public unidad As Integer
+    Public descripcion As String
+    Public id_producto As Integer
+    Public precio As Decimal
+    Public nombreUnidad As String
+    Sub AbrirConexion()
+        Try
+            con = New SqlConnection("Data Source=ANNONYMOUS\SQLEXPRESS;database=BDferreteria; Integrated Security=True")
+            con.Open()
+        Catch ex As Exception
+            MsgBox("No se pudo conectar" + ex.ToString)
+        End Try
+    End Sub
+End Module
---
+++ ImprimirFactura.vb
@@ -0,0 +1,16 @@
+﻿Imports Microsoft.ReportViewer.Common
+
+Imports Microsoft.ReportViewer.ProcessingObjectModel
+
+Imports Microsoft.ReportViewer.WebForms
+
+Imports Microsoft.ReportViewer.WinForms
+
+Imports Microsoft.ReportViewer.DataVisualization
+Imports System.IO
+Public Class ImprimirFactura
+
+    Private Sub ImprimirFactura_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
